21d19bf82b0a33d396fa10a7421e2cceb674a559
zfearnside <zrfearnside01@gmail.com>
2018-01-16 17:26:35 -0700

    Programmed Gripper + Zanathyr's Grasp

--- src/main/java/frc/team1245/robot/subsystems/Gripper.java
+++ src/main/java/frc/team1245/robot/subsystems/Gripper.java
@@ -1,11 +1,20 @@
 package frc.team1245.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.team1245.robot.commands.ZanathyrsGrasp;
 
 public class Gripper extends Subsystem{
 
+    public WPI_TalonSRX leftArmOfZanathyr, rightArmOfZanathyr;
+
+    public Gripper(int leftArm, int rightArm) {
+        this.leftArmOfZanathyr = new WPI_TalonSRX(leftArm);
+        this.rightArmOfZanathyr = new WPI_TalonSRX(rightArm);
+    }
+
     @Override
     protected void initDefaultCommand() {
-
+        setDefaultCommand(new ZanathyrsGrasp());
     }
 }
